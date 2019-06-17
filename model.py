bd5367abd2e48876d1ecc549cbf75c30e021f596
kodamanbou <kodamanbou0424@gmail.com>
2019-06-18 08:13:59 +0900

    Create networks.

--- model.py
+++ model.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 import numpy as np
-import MeCab
 from text_encoder import JapaneseTextEncoder
-import os, sys, codecs
+import os
 import librosa
+import copy
+import signal
+from hyperparams import Hyperparams as hp
+from network import encoder, decoder1, decoder2, embed
 
 
 def make_dataset(path):
@@ -15,24 +18,70 @@ def make_dataset(path):
             corpus.append(text)
 
     print(corpus)
-    encoder = JapaneseTextEncoder(corpus, append_eos=True, maxlen=50, padding=True)
+    encoder = JapaneseTextEncoder(corpus, append_eos=True)
     encoder.build()
 
-    return fpaths, encoder.dataset
+    text_lengths = [len(words) for words in encoder.dataset]
+
+    return fpaths, text_lengths, encoder.dataset
+
+
+def spectrogram2wav(mag):
+    '''# Generate wave file from spectrogram'''
+    # transpose
+    mag = mag.T
+
+    # de-noramlize
+    mag = (np.clip(mag, 0, 1) * hp.max_db) - hp.max_db + hp.ref_db
+
+    # to amplitude
+    mag = np.power(10.0, mag * 0.05)
+
+    # wav reconstruction
+    wav = griffin_lim(mag)
+
+    # de-preemphasis
+    wav = signal.lfilter([1], [1, -hp.preemphasis], wav)
+
+    # trim
+    wav, _ = librosa.effects.trim(wav)
+
+    return wav.astype(np.float32)
+
+
+def griffin_lim(spectrogram):
+    X_best = copy.deepcopy(spectrogram)
+    for i in range(hp.n_iter):
+        X_t = invert_spectrogram(X_best)
+        est = librosa.stft(X_t, hp.n_fft, hp.hop_length, win_length=hp.win_length)
+        phase = est / np.maximum(1e-8, np.abs(est))
+        X_best = spectrogram * phase
+
+    X_t = invert_spectrogram(X_best)
+    y = np.real(X_t)
+
+    return y
+
+
+def invert_spectrogram(spectrogram):
+    '''
+    spectrogram: [f, t]
+    '''
+    return librosa.istft(spectrogram, hp.hop_length, win_length=hp.win_length, window="hann")
 
 
 def get_spectrograms(fpath):
-    y, sr = librosa.load(fpath, sr=22050)
+    y, sr = librosa.load(fpath, sr=hp.sr)
     y, _ = librosa.effects.trim(y)
-    y = np.append(y[0], y[1:] - 0.97 * y[:-1])
+    y = np.append(y[0], y[1:] - hp.preemphasis * y[:-1])
 
     linear = librosa.stft(y=y,
-                          n_fft=2048,
-                          hop_length=int(sr * 0.0125),
-                          win_length=int(sr * 0.05))
+                          n_fft=hp.n_fft,
+                          hop_length=hp.hop_length,
+                          win_length=hp.win_length)
 
     mag = np.abs(linear)
-    mel_basis = librosa.filters.mel(sr, 2048, 80)
+    mel_basis = librosa.filters.mel(hp.sr, hp.n_fft, hp.n_mels)
     mel = np.dot(mel_basis, mag)
 
     # to decibel.
@@ -40,8 +89,8 @@ def get_spectrograms(fpath):
     mag = 20 * np.log10(np.maximum(1e-5, mag))
 
     # normalize.
-    mel = np.clip((mel - 20 + 100) / 100, 1e-8, 1)
-    mag = np.clip((mag - 20 + 100) / 100, 1e-8, 1)
+    mel = np.clip((mel - hp.ref_db + hp.max_db) / hp.max_db, 1e-8, 1)
+    mag = np.clip((mag - hp.ref_db + hp.max_db) / hp.max_db, 1e-8, 1)
 
     # transpose.
     mel = mel.T.astype(np.float32)
@@ -54,34 +103,23 @@ def load_spectrograms(fpath):
     fname = os.path.basename(fpath)
     mel, mag = get_spectrograms(fpath)
     t = mel.shape[0]
-    num_paddings = 5 - (t % 5) if t % 5 != 0 else 0  # for reduction
+    num_paddings = hp.r - (t % hp.r) if t % hp.r != 0 else 0  # for reduction
     mel = np.pad(mel, [[0, num_paddings], [0, 0]], mode="constant")
     mag = np.pad(mag, [[0, num_paddings], [0, 0]], mode="constant")
     return fname, mel.reshape((-1, 80 * 5)), mag
 
 
-def embed(inputs, vocab_size, num_units, zero_pad=True, scope='embedding', reuse=None):
-    with tf.variable_scope(scope, reuse=reuse):
-        lookup_table = tf.get_variable('lookup_table',
-                                       dtype=tf.float32,
-                                       shape=[vocab_size, num_units],
-                                       initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.01))
-
-        if zero_pad:
-            lookup_table = tf.concat((tf.zeros(shape=[1, num_units]),
-                                      lookup_table[1:, :]), 0)
-
-    return tf.nn.embedding_lookup(lookup_table, inputs)
-
-
-def get_batch(batch_size=1):
+def get_batch():
     with tf.device('/cpu:0'):
-        fpaths, dataset = make_dataset('datasets/sentences.dat')
-        num_batch = len(fpaths) // batch_size
+        fpaths, text_lengths, dataset = make_dataset('datasets/sentences.dat')
+        max_len, min_len = max(text_lengths), min(text_lengths)
+
+        num_batch = len(fpaths) // hp.batch_size
         fpaths = tf.convert_to_tensor(fpaths)
+        text_lengths = tf.convert_to_tensor(text_lengths)
         dataset = tf.convert_to_tensor(dataset)
 
-        fpath, text = tf.train.slice_input_producer([fpaths, dataset], shuffle=True)
+        fpath, text = tf.train.slice_input_producer([fpaths, text_lengths, dataset], shuffle=True)
 
         fname, mel, mag = tf.py_function(load_spectrograms, [fpath], [tf.string, tf.float32, tf.float32])
 
@@ -93,9 +131,10 @@ def get_batch(batch_size=1):
         _, (texts, mels, mags, fnames) = tf.contrib.training.bucket_by_sequence_length(
             input_length=50,
             tensors=[text, mel, mag, fname],
-            batch_size=batch_size,
+            batch_size=hp.batch_size,
+            bucket_boundaries=[i for i in range(min_len + 1, max_len - 1, 20)],
             num_threads=16,
-            capacity=batch_size * 4,
+            capacity=hp.batch_size * 4,
             dynamic_pad=True
         )
 
@@ -107,15 +146,30 @@ class Graph:
         is_training = True if mode == 'train' else False
 
         if mode == 'train':
-            self.x, self.y, self.z, self.fnames, self.num_batch = get_batch(16)
+            self.x, self.y, self.z, self.fnames, self.num_batch = get_batch()
         elif mode == 'eval':
             self.x = tf.placeholder(tf.int32, shape=(None, None))
-            self.y = tf.placeholder(tf.float32, shape=(None, None, 80 * 5))
-            self.z = tf.placeholder(tf.float32, shape=(None, None, 1 + 2048 // 2))
+            self.y = tf.placeholder(tf.float32, shape=(None, None, hp.n_mels * hp.r))
+            self.z = tf.placeholder(tf.float32, shape=(None, None, 1 + hp.n_fft // 2))
             self.fnames = tf.placeholder(tf.string, shape=(None,))
         else:
+            # synthesize.
             self.x = tf.placeholder(tf.int32, shape=(None, None))
-            self.y = tf.placeholder(tf.float32, shape=(None, None, 80 * 5))
+            self.y = tf.placeholder(tf.float32, shape=(None, None, hp.n_mels * hp.r))
+
+        self.encoder_inputs = embed(self.x, hp.vocab_size, hp.embed_size)
+        self.decoder_inputs = tf.concat((tf.zeros_like(self.y[:, :1, :]), self.y[:, :-1, :]), 1)
+        self.decoder_inputs = self.decoder_inputs[:, :, -hp.n_mels:]
+
+        # Network.
+        with tf.variable_scope("net"):
+            self.memory = encoder(self.encoder_inputs, is_training=is_training)
+            self.y_hat, self.alignments = decoder1(self.decoder_inputs,
+                                                   self.memory,
+                                                   is_training=is_training)
+            self.z_hat = decoder2(self.y_hat, is_training=is_training)
+
+        self.audio = tf.py_function(spectrogram2wav, [self.z_hat[0]], tf.float32)
 
 
 if __name__ == '__main__':
---
+++ network.py
@@ -0,0 +1,244 @@
+from hyperparams import Hyperparams as hp
+import tensorflow as tf
+
+
+def embed(inputs, vocab_size, num_units, zero_pad=True, scope='embedding', reuse=None):
+    with tf.variable_scope(scope, reuse=reuse):
+        lookup_table = tf.get_variable('lookup_table',
+                                       dtype=tf.float32,
+                                       shape=[vocab_size, num_units],
+                                       initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.01))
+
+        if zero_pad:
+            lookup_table = tf.concat((tf.zeros(shape=[1, num_units]),
+                                      lookup_table[1:, :]), 0)
+
+    return tf.nn.embedding_lookup(lookup_table, inputs)
+
+
+def bn(inputs,
+       is_training=True,
+       activation=None,
+       scope='bn',
+       reuse=None):
+    inputs_shape = inputs.get_shape()
+    inputs_rank = inputs_shape.shape.ndims
+
+    if inputs_rank in [2, 3, 4]:
+        if inputs_rank == 2:
+            inputs = tf.expand_dims(inputs, axis=1)
+            inputs = tf.expand_dims(inputs, axis=2)
+        elif inputs_rank == 3:
+            inputs = tf.expand_dims(inputs, axis=1)
+
+        outputs = tf.contrib.layers.batch_norm(inputs=inputs,
+                                               center=True,
+                                               scale=True,
+                                               updates_collections=None,
+                                               is_training=is_training,
+                                               scope=scope,
+                                               fused=True,
+                                               reuse=reuse)
+
+        if inputs_rank == 2:
+            outputs = tf.squeeze(outputs, axis=[1, 2])
+        elif inputs_rank == 3:
+            outputs = tf.squeeze(outputs, axis=1)
+
+    else:
+        outputs = tf.contrib.layers.batch_norm(inputs=inputs,
+                                               center=True,
+                                               scale=True,
+                                               updates_collections=None,
+                                               is_training=is_training,
+                                               scope=scope,
+                                               fused=False,
+                                               reuse=reuse)
+
+    if activation is not None:
+        outputs = activation(outputs)
+
+    return outputs
+
+
+def conv1d(inputs,
+           filters=None,
+           size=1,
+           rate=1,
+           padding="SAME",
+           use_bias=False,
+           activation=None,
+           scope="conv1d",
+           reuse=None):
+    with tf.variable_scope(scope):
+        if padding.lower() == "causal":
+            # pre-padding for causality
+            pad_len = (size - 1) * rate  # padding size
+            inputs = tf.pad(inputs, [[0, 0], [pad_len, 0], [0, 0]])
+            padding = "valid"
+
+        if filters is None:
+            filters = inputs.get_shape().as_list[-1]
+
+        params = {"inputs": inputs, "filters": filters, "kernel_size": size,
+                  "dilation_rate": rate, "padding": padding, "activation": activation,
+                  "use_bias": use_bias, "reuse": reuse}
+
+        outputs = tf.layers.conv1d(**params)
+    return outputs
+
+
+def conv1d_banks(inputs, K=16, is_training=True, scope="conv1d_banks", reuse=None):
+    with tf.variable_scope(scope, reuse=reuse):
+        outputs = conv1d(inputs, hp.embed_size // 2, 1)  # k=1
+        for k in range(2, K + 1):  # k = 2...K
+            with tf.variable_scope("num_{}".format(k)):
+                output = conv1d(inputs, hp.embed_size // 2, k)
+                outputs = tf.concat((outputs, output), -1)
+        outputs = bn(outputs, is_training=is_training, activation=tf.nn.relu)
+    return outputs  # (N, T, Hp.embed_size//2*K)
+
+
+def gru(inputs, num_units=None, bidirection=False, scope="gru", reuse=None):
+    with tf.variable_scope(scope, reuse=reuse):
+        if num_units is None:
+            num_units = inputs.get_shape().as_list[-1]
+
+        cell = tf.contrib.rnn.GRUCell(num_units)
+        if bidirection:
+            cell_bw = tf.contrib.rnn.GRUCell(num_units)
+            outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell, cell_bw, inputs, dtype=tf.float32)
+            return tf.concat(outputs, 2)
+        else:
+            outputs, _ = tf.nn.dynamic_rnn(cell, inputs, dtype=tf.float32)
+            return outputs
+
+
+def attention_decoder(inputs, memory, num_units=None, scope="attention_decoder", reuse=None):
+    with tf.variable_scope(scope, reuse=reuse):
+        if num_units is None:
+            num_units = inputs.get_shape().as_list[-1]
+
+        attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(num_units,
+                                                                   memory)
+        decoder_cell = tf.contrib.rnn.GRUCell(num_units)
+        cell_with_attention = tf.contrib.seq2seq.AttentionWrapper(decoder_cell,
+                                                                  attention_mechanism,
+                                                                  num_units,
+                                                                  alignment_history=True)
+        outputs, state = tf.nn.dynamic_rnn(cell_with_attention, inputs, dtype=tf.float32)  # ( N, T', 16)
+
+    return outputs, state
+
+
+def prenet(inputs, num_units=None, is_training=True, scope="prenet", reuse=None):
+    if num_units is None:
+        num_units = [hp.embed_size, hp.embed_size // 2]
+
+    with tf.variable_scope(scope, reuse=reuse):
+        outputs = tf.layers.dense(inputs, units=num_units[0], activation=tf.nn.relu, name="dense1")
+        outputs = tf.layers.dropout(outputs, rate=hp.dropout_rate, training=is_training, name="dropout1")
+        outputs = tf.layers.dense(outputs, units=num_units[1], activation=tf.nn.relu, name="dense2")
+        outputs = tf.layers.dropout(outputs, rate=hp.dropout_rate, training=is_training, name="dropout2")
+    return outputs  # (N, ..., num_units[1])
+
+
+def highwaynet(inputs, num_units=None, scope="highwaynet", reuse=None):
+    if not num_units:
+        num_units = inputs.get_shape()[-1]
+
+    with tf.variable_scope(scope, reuse=reuse):
+        H = tf.layers.dense(inputs, units=num_units, activation=tf.nn.relu, name="dense1")
+        T = tf.layers.dense(inputs, units=num_units, activation=tf.nn.sigmoid,
+                            bias_initializer=tf.constant_initializer(-1.0), name="dense2")
+        outputs = H * T + inputs * (1. - T)
+    return outputs
+
+
+# encoder/decoder networks.
+def encoder(inputs, is_training=True, scope="encoder", reuse=None):
+    with tf.variable_scope(scope, reuse=reuse):
+        # Encoder pre-net
+        prenet_out = prenet(inputs, is_training=is_training)  # (N, T_x, E/2)
+
+        # Encoder CBHG
+        # Conv1D banks
+        enc = conv1d_banks(prenet_out, K=hp.encoder_num_banks, is_training=is_training)  # (N, T_x, K*E/2)
+
+        # Max pooling
+        enc = tf.layers.max_pooling1d(enc, pool_size=2, strides=1, padding="same")  # (N, T_x, K*E/2)
+
+        # Conv1D projections
+        enc = conv1d(enc, filters=hp.embed_size // 2, size=3, scope="conv1d_1")  # (N, T_x, E/2)
+        enc = bn(enc, is_training=is_training, activation=tf.nn.relu, scope="conv1d_1")
+
+        enc = conv1d(enc, filters=hp.embed_size // 2, size=3, scope="conv1d_2")  # (N, T_x, E/2)
+        enc = bn(enc, is_training=is_training, scope="conv1d_2")
+
+        enc += prenet_out  # (N, T_x, E/2) # residual connections
+
+        # Highway Nets
+        for i in range(hp.num_highwaynet_blocks):
+            enc = highwaynet(enc, num_units=hp.embed_size // 2,
+                             scope='highwaynet_{}'.format(i))  # (N, T_x, E/2)
+
+        # Bidirectional GRU
+        memory = gru(enc, num_units=hp.embed_size // 2, bidirection=True)  # (N, T_x, E)
+
+    return memory
+
+
+def decoder1(inputs, memory, is_training=True, scope="decoder1", reuse=None):
+    with tf.variable_scope(scope, reuse=reuse):
+        # Decoder pre-net
+        inputs = prenet(inputs, is_training=is_training)  # (N, T_y/r, E/2)
+
+        # Attention RNN
+        dec, state = attention_decoder(inputs, memory, num_units=hp.embed_size)  # (N, T_y/r, E)
+
+        # for attention monitoring
+        alignments = tf.transpose(state.alignment_history.stack(), [1, 2, 0])
+
+        # Decoder RNNs
+        dec += gru(dec, hp.embed_size, bidirection=False, scope="decoder_gru1")  # (N, T_y/r, E)
+        dec += gru(dec, hp.embed_size, bidirection=False, scope="decoder_gru2")  # (N, T_y/r, E)
+
+        # Outputs => (N, T_y/r, n_mels*r)
+        mel_hats = tf.layers.dense(dec, hp.n_mels * hp.r)
+
+    return mel_hats, alignments
+
+
+def decoder2(inputs, is_training=True, scope="decoder2", reuse=None):
+    with tf.variable_scope(scope, reuse=reuse):
+        # Restore shape -> (N, Ty, n_mels)
+        inputs = tf.reshape(inputs, [tf.shape(inputs)[0], -1, hp.n_mels])
+
+        # Conv1D bank
+        dec = conv1d_banks(inputs, K=hp.decoder_num_banks, is_training=is_training)  # (N, T_y, E*K/2)
+
+        # Max pooling
+        dec = tf.layers.max_pooling1d(dec, pool_size=2, strides=1, padding="same")  # (N, T_y, E*K/2)
+
+        # Conv1D projections
+        dec = conv1d(dec, filters=hp.embed_size // 2, size=3, scope="conv1d_1")  # (N, T_x, E/2)
+        dec = bn(dec, is_training=is_training, activation=tf.nn.relu, scope="conv1d_1")
+
+        dec = conv1d(dec, filters=hp.n_mels, size=3, scope="conv1d_2")  # (N, T_x, E/2)
+        dec = bn(dec, is_training=is_training, scope="conv1d_2")
+
+        # Extra affine transformation for dimensionality sync
+        dec = tf.layers.dense(dec, hp.embed_size // 2)  # (N, T_y, E/2)
+
+        # Highway Nets
+        for i in range(4):
+            dec = highwaynet(dec, num_units=hp.embed_size // 2,
+                             scope='highwaynet_{}'.format(i))  # (N, T_y, E/2)
+
+        # Bidirectional GRU
+        dec = gru(dec, hp.embed_size // 2, bidirection=True)  # (N, T_y, E)
+
+        # Outputs => (N, T_y, 1+n_fft//2)
+        outputs = tf.layers.dense(dec, 1 + hp.n_fft // 2)
+
+    return outputs
