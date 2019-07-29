4c1c44db5d930b0c8ce25e19d1c5c540c9ce4a2b
kodamanbou <kodamanbou0424@gmail.com>
2019-07-29 10:06:06 +0900

    Distributed version.

--- hyperparams.py
+++ hyperparams.py
@@ -33,10 +33,10 @@ class Hyperparams:
 
     cluster_spec = tf.train.ClusterSpec({
         'ps': [
-            '192.168.1.12:2221',  # /job:ps/task:0
-            '192.168.1.10:2221'  # /job:ps/task:1
+            '192.168.1.12:2221'  # /job:ps/task:0
         ],
         'worker': [
-            '192.168.1.6:2222'  # /job:worker/task:0 (Docker Image)
+            '192.168.1.6:2222',  # /job:worker/task:0 (Docker Image)
+            '192.168.1.10:2221'  # /job:worker/task:1
         ]
     })
