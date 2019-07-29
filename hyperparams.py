d7cee8389058997a536ed4c1bae56c902f362d30
kodamanbou <kodamanbou0424@gmail.com>
2019-07-29 10:06:29 +0900

    Distributed version.

--- hyperparams.py
+++ hyperparams.py
@@ -37,6 +37,6 @@ class Hyperparams:
         ],
         'worker': [
             '192.168.1.6:2222',  # /job:worker/task:0 (Docker Image)
-            '192.168.1.10:2221'  # /job:worker/task:1
+            '192.168.1.10:2222'  # /job:worker/task:1
         ]
     })
